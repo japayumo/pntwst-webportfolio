96a003d19addf98de1097e06b4809cc7b9dd8905
jose.payumo <jose.payumo@pointwest.com.ph>
2023-09-05 17:33:31 +0800

    Added s40 activity files

---
+++ SQL/s40/solution.sql
@@ -0,0 +1,31 @@
+1. Create an activity folder and a solution.sql file inside of it.
+2. Given the following data, provide the answer to the following:
+a. List the books Authored by Marjorie Green.
+	a. Marjorie Green
+		1.BU1032 - The Busy Executive's Database
+		2. BU2075 -You Can Combat Computer Stress
+b. List the books Authored by Michael O'Leary.
+	b. Michael O' Leary
+		1. BU1111 - Cooking with Computers
+		2. TC7777 - 
+C. Write the author/s of "The Busy Executives Database Guide".
+	c. Author/s of the "The Busy Executives Database Guide" (BU1032)
+		1. Marjorie Green
+		2. Abraham Bennet
+d. Identify the publisher of "But Is It User Friendly?".
+	d. Publisher of "But Is It User Friendly" (PC1035)
+	1. 1389 - Algodata Infosystems
+
+e.List the books published by Algodata Infosystems.
+	e. Books Published by Algodata Infosystems
+		1. The Busy Executive's Database
+		2. Cooking with Computers
+		3. Straight Talk About Computers
+		4. But Is It User Friendly
+		5. Secrets of Silicon Valley
+		6. Net Etiquette
+	
+4. Create a database design an and ERD for an airline reservation system using Google drawings. Create tables for the following:
+a. Users
+b. Bookings
+c. Flights
