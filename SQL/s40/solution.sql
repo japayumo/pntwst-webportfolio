33ea3e84cacd676830eb80b17c529c424745f94d
jose.payumo <jose.payumo@pointwest.com.ph>
2023-09-05 18:02:39 +0800

    Revised s40 activity files

--- SQL/s40/solution.sql
+++ SQL/s40/solution.sql
@@ -7,7 +7,7 @@ a. List the books Authored by Marjorie Green.
 b. List the books Authored by Michael O'Leary.
 	b. Michael O' Leary
 		1. BU1111 - Cooking with Computers
-		2. TC7777 - 
+		2. TC7777 - N/A
 C. Write the author/s of "The Busy Executives Database Guide".
 	c. Author/s of the "The Busy Executives Database Guide" (BU1032)
 		1. Marjorie Green
@@ -24,7 +24,7 @@ e.List the books published by Algodata Infosystems.
 		4. But Is It User Friendly
 		5. Secrets of Silicon Valley
 		6. Net Etiquette
-	
+
 4. Create a database design an and ERD for an airline reservation system using Google drawings. Create tables for the following:
 a. Users
 b. Bookings
